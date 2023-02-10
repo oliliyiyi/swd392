840d46d2a6bb498d2436dc8ad686bd0e28e68cd2
Trinh Binh Minh <minhtbse151276@fpt.edu.vn>
2023-02-10 15:44:11 +0700

    @Type/node and express

--- index.ts
+++ index.ts
@@ -26,7 +26,12 @@ const swaggerDoc = swaggerJSDoc(options);
 
 app.use(express.json());
 
-
+// app.use(function(req, res, next)  {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
+//     next();
+// });
 
 app.use(router,swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
