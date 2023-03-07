19d4d076ff730510ae180dcc0eed803707dd9d59
Trinh Binh Minh <minhtbse151276@fpt.edu.vn>
2023-03-07 14:51:29 +0700

    UpdateLoginV3

--- index.ts
+++ index.ts
@@ -42,17 +42,26 @@ const options = {
 }
 
 const swaggerDoc = swaggerJSDoc(options);
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+app.use(express.json());
+app.use(function (req: any, res: any, next: any) {
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
+
+
+
+
+
+
 app.use(router,swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:3000`);
