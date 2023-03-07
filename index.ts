95b82fd90d1c545330d0fafc6150d0e302fe56fa
Trinh Binh Minh <minhtbse151276@fpt.edu.vn>
2023-03-07 13:10:26 +0700

    access-Control-Allow

--- index.ts
+++ index.ts
@@ -42,7 +42,7 @@ const options = {
 }
 
 const swaggerDoc = swaggerJSDoc(options);
-
+app.use(express.json());
 app.use(function (req: any, res: any, next: any) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
@@ -54,7 +54,7 @@ app.use(function (req: any, res: any, next: any) {
 });
 
 
-app.use(express.json());
+
 
 
 
